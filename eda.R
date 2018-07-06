14a9df0411051fab2071d6391393723fb5630233
Raul Carlomagno <rcarlomagno@gmail.com>
2018-07-05 23:31:19 -0300

    first

---
+++ eda.R
@@ -0,0 +1,17 @@
+library(data.table)
+
+d <- fread("Shark_Attack_Data.csv")
+
+dim(d)
+
+View(d)
+
+table(d$Type)/sum(table(d$Type))
+table(d$Year)/sum(table(d$Year))
+table(d$Sex)/sum(table(d$Sex))
+table(d$Age)/sum(table(d$Age))
+table(d$`Fatal (Y/N)`)/sum(table(d$`Fatal (Y/N)`))
+
+length(unique(d$Country))
+length(unique(d$Location))
+length(unique(d$Area))
